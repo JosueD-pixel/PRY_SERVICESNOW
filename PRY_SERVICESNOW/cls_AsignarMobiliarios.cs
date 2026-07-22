6e93cb1ae65b034257f5e7513c8dd2f7f1835fb5
Angel Josue Domingo Guillen <angel_domingoguillen07@hotmail.com>
2026-07-21 20:12:34 -0600

    kkk

---
+++ PRY_SERVICESNOW/cls_AsignarMobiliarios.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRY_SERVICESNOW
+{
+    internal class cls_AsignarMobiliarios
+    {
+    }
+}
